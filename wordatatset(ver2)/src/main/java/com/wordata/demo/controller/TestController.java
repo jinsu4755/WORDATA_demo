4aecbd607941471af0ce457218a19b145cd3e39b
jinsu4755 <parkjinsu4755@daum.net>
2020-03-17 19:15:40 +0900

    test add

--- wordatatset(ver2)/src/main/java/com/wordata/demo/controller/TestController.java
+++ wordatatset(ver2)/src/main/java/com/wordata/demo/controller/TestController.java
@@ -1,34 +1,28 @@
 package com.wordata.demo.controller;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.jws.WebParam;
 
 @Controller
 @RequestMapping("test")
 public class TestController {
 
     @RequestMapping("index")
-    public String testIndex(){
-        return "test/index";
+    public String test(Model model){
+        model.addAttribute("string1", "testString 1");
+        model.addAttribute("string2", "testString2");
+        return "test/test";
     }
 
-    @RequestMapping("join")
-    public String testJoin(){
-        return "test/joinMembership";
-    }
-
-    @RequestMapping("login")
-    public String testLogin(){
-        return "test/login";
-    }
-
-    @RequestMapping("service")
-    public String testService(){
-        return "test/service";
-    }
-
-    @RequestMapping("faq")
-    public String testFaq(){
-        return "test/faq";
+    @RequestMapping("param")
+    public String testParam(@RequestParam("id") String id, Model model) {
+        model.addAttribute("id", id);
+        return "test/testParam";
     }
 }
